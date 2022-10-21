8f4aa602c8c00a0b8c9c9fe7112ab9ee3bff47ba
marcelmacedo <marcel.handock@gmail.com>
2022-10-21 10:03:34 -0400

    first commit

---
+++ src/main/java/com/algaworks/algalog/api/assembler/EntregaAssembler.java
@@ -0,0 +1,38 @@
+package com.algaworks.algalog.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algalog.api.model.EntregaModel;
+import com.algaworks.algalog.api.model.input.EntregaInput;
+import com.algaworks.algalog.domain.model.Entrega;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component				// Indica que sera gerenciado pelo spring.
+public class EntregaAssembler {
+/*
+	Esta classe é responsavel por fazer o mapeamento e montagem dos objetos para converter
+	de um tipo para outro. 
+*/
+	private ModelMapper modelMapper;	// Variavel de instancia.
+	
+	public EntregaModel toModel(Entrega entrega) {
+		return modelMapper.map(entrega, EntregaModel.class);
+	}
+	
+	public List<EntregaModel> toCollectionModel(List<Entrega> entregas) {
+		return entregas.stream()
+				.map(this::toModel)
+				.collect(Collectors.toList());
+	}
+	
+	public Entrega toEntity(EntregaInput entregaInput) {
+		return modelMapper.map(entregaInput, Entrega.class);
+	}
+	
+}
---
+++ src/main/java/com/algaworks/algalog/domain/model/Destinatario.java
@@ -0,0 +1,35 @@
+package com.algaworks.algalog.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable // Indica que a classe pode ser usada como @Embedded em outra classe.
+public class Destinatario {
+
+	@NotBlank
+	@Column(name = "destinatario_nome") // Customização do nome da coluna na tabela.
+	private String nome;
+	
+	@NotBlank
+	@Column(name = "destinatario_logradouro")
+	private String logradouro;
+	
+	@NotBlank
+	@Column(name = "destinatario_numero")
+	private String numero;
+	
+	@NotBlank
+	@Column(name = "destinatario_complemento")
+	private String complemento;
+	
+	@NotBlank
+	@Column(name = "destinatario_bairro")
+	private String bairro;
+	
+}
